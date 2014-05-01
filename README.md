00459b6b5081369241f7c7ecdd4ee39f55869ddb
Johniel <johniel.s.m@gmail.com>
2014-05-01 11:18:47 +0900

    add example

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# Switch Cursor Color
 
+## Example
+
+    (add-to-list 'scc-color-detector '(lambda () "green"))
+
+    (require 'tramp)
+    (defun get-tramp-hostname ()
+      (let ((name (buffer-file-name)))
+        (if (and name (tramp-tramp-file-p name))
+            (tramp-file-name-real-host (tramp-dissect-file-name name)))))
+    (add-to-list 'scc-color-detector '(lambda ()
+                                        (let ((hostname (get-tramp-hostname)))
+                                          (cond ((member hostname '("AAA" "BBB")) "red")
+                                                ((member hostname '("CCC" "DDD"))  "deep sky blue")
+                                                (t nil)))))
--- scc.el
+++ scc.el
@@ -1,5 +1,4 @@
 (require 'dash)
-(require 'tramp)
 
 (defvar scc-default-color "purple2")
 
@@ -17,17 +16,6 @@
 (defadvice switch-to-buffer (after scc-switch-to-buffer disable)
   (set-cursor-color (scc-get-cursor-color))
   ad-return-value)
-
-(defun get-tramp-hostname ()
-  (let ((name (buffer-file-name)))
-    (if (and name (tramp-tramp-file-p name))
-        (tramp-file-name-real-host (tramp-dissect-file-name name)))))
-
-(add-to-list 'scc-color-detector '(lambda ()
-                                    (let ((hostname (get-tramp-hostname)))
-                                      (cond ((member hostname '()) "red")
-                                            ((member hostname '())  "deep sky blue")
-                                            (t nil)))))
 
 ;;;###autoload
 (define-minor-mode scc-mode
